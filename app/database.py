b0e762c53c1308c8e1696e05e0bd9043dc6df355
IvanLauLinTiong <ivanlau91@hotmail.com>
2024-07-17 19:05:56 +0800

    interim commit

--- app/database.py
+++ app/database.py
@@ -6,7 +6,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- app/main.py
+++ app/main.py
@@ -6,8 +6,13 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.templating import Jinja2Templates
 
 from schemas import TaskResponse, TranslationRequest
+from database import get_db, engine
+from crud import create_translation_task, get_translation_task, update_translation_task
+import models
 
 
+
+models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 BASE_DIR = pathlib.Path(__file__).parent
@@ -15,9 +20,6 @@ BASE_DIR = pathlib.Path(__file__).parent
 # setup templates
 templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 
-@app.get("/index", response_class=HTMLResponse)
-def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
 
 # Enable CORS (need to adjust accordingly if deploy in prod)
 app.add_middleware(
@@ -28,11 +30,14 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
+@app.get("/index", response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 @app.post("/translate", response_model=TaskResponse)
 def translate(request: TranslationRequest, background_tasks: BackgroundTasks):
 
-    task = create_translation_task(...)
+    task = create_translation_task(get_db(), request.text, request.languages)
 
     background_tasks.add_task(perform_translation, task.id, request.text, request.languages, db)
 
